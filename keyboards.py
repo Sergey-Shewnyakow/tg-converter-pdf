dfb4d0e73aed018a3d46d977bddee510b59f2fe9
Vasilii Semenov <ivaso2004@mail.ru>
2024-04-12 22:37:40 +0300

    finish ui

--- keyboards.py
+++ keyboards.py
@@ -1,8 +1,17 @@
 from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove, \
     ReplyKeyboardMarkup
+from aiogram import types
 
 
-# def get_start_kb() -> ReplyKeyboardMarkup:
-#     kb = ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard=True)
-#     kb.add(KeyboardButton(text="Я администратор!"), KeyboardButton(text="Я студент!"))
-#     return kb
+def get_start_kb() -> ReplyKeyboardMarkup:
+    kb = [
+        [types.KeyboardButton(text="Преобразовать в pdf")],
+        [types.KeyboardButton(text="Объединить pdf")]
+    ]
+    keyboard = types.ReplyKeyboardMarkup(
+        keyboard=kb,
+        resize_keyboard=True,
+        input_field_placeholder="Выберите действие",
+        one_time_keyboard=True
+    )
+    return keyboard
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from config import BOT_API_KEY
 from config import CONVERTER_API_KEY
 import asyncio
-from aiogram import Bot, types, Dispatcher
-from aiogram.enums import ParseMode
+from aiogram import Dispatcher, Bot, types
+from aiogram.filters import CommandStart
+from keyboards import get_start_kb
 
 
+bot = Bot(token=BOT_API_KEY)
 dp = Dispatcher()
 
 
-@dp.message()
-async def start_cmd(message: types.Message) -> None:
-    await message.answer('Привет!')
+@dp.message(CommandStart())
+async def start_cmd(message: types.Message):
+    await bot.send_message(chat_id=message.from_user.id,
+                           text='Добро пожаловать в нашего бота! Выберите действие!',
+                           reply_markup=get_start_kb())
 
 
-async def main() -> None:
-    bot = Bot(BOT_API_KEY, parse_mode=ParseMode.HTML)
+@dp.message(lambda message: message.text in ['Преобразовать в pdf', 'Объединить pdf'])
+async def convert_or_merge(message: types.Message):
+    if message.text == "Преобразовать в pdf":
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Отправьте файл в формате docx, xlsx, txt и тд")
+    elif message.text == "Объединить pdf":
+        await bot.send_message(chat_id=message.from_user.id,
+                               text="Отправьте несколько pdf файлов одним сообщением")
+
+
+
+async def main():
     await dp.start_polling(bot)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+asyncio.run(main())
+
+
